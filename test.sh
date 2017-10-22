3b2bd53e18fcc9aabfa08eeba87f1acb5debb030
Jeff <woz5999@gmail.com>
2017-10-22 15:01:43 -0700

    update build script

--- test.sh
+++ test.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 docker stop node-manager || true &&
 docker rm node-manager || true &&
-# docker rmi node-manager || true &&
-# ./build.sh &&
 export NODEMAN_AWS_REGION=us-east-1
 export NODEMAN_AWS_SQS_QUEUE_URL='https://sqs.us-east-1.amazonaws.com/282028653949/scaling-test'
 export NODEMAN_DEBUG=true
